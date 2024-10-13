419ca3ae3ed79d0a030f1b49e9242e10885a610a
Kealan <kd0111@uah.edu>
2024-10-12 21:40:15 -0500

    updated?

--- src/main/java/blackjack/twist/Card.java
+++ src/main/java/blackjack/twist/Card.java
@@ -1,48 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package blackjack.twist;
 
-/**
- * Card Class
- * @author vasilisa
- */
-public class Card 
+import java.util.Random;
+
+public class Card
 {
-    enum Suit 
+    enum Suit
     {
         CLUBS, DIAMONDS, HEARTS, SPADES
     }
-    
+
     private Suit suit;
     private int rank;
-    
+
     /**
      * Constructor
+     * @param suit - the suit of the card
+     * @param rank - the rank of the card (1 = Ace, 11 = Jack, 12 = Queen, 13 = King)
      */
-    public Card()
+    public Card(Suit suit, int rank)
     {
-        rank = 0;
+        this.suit = suit;
+        this.rank = rank;
     }
-    
+
     /**
-     * generateCard()
-     * @return - returns Card object that is generated
+     * @return - a new random Card object
      */
     public Card generateCard()
     {
-        Card card = new Card();
-        return card;
+        Random random = new Random();
+        Suit[] suits = Suit.values();
+        Suit randomSuit = suits[random.nextInt(suits.length)];
+        int randomRank = random.nextInt(13) + 1;  // Ranks from 1 to 13
+
+        return new Card(randomSuit, randomRank);
     }
-    
+
     /**
-     * compareCard()
-     * @return - returns int based on comparison
+     * @param otherCard - the card to compare to
+     * @return - positive if this card has a higher rank, negative if lower, and 0 if equal
      */
-    public int compareCard()
+    public int compareCard(Card otherCard)
     {
-        return 0;
+        return Integer.compare(this.rank, otherCard.rank);
     }
-    
+
+    /**
+     * @return - the suit of the card
+     */
+    public Suit getSuit() {
+        return suit;
+    }
+
+    /**
+     * @return - the rank of the card
+     */
+    public int getRank() {
+        return rank;
+    }
+
+    /**
+     * @return - string representation of the card
+     */
+    @Override
+    public String toString() {
+        String[] rankNames = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        return rankNames[rank - 1] + " of " + suit;
+    }
 }
